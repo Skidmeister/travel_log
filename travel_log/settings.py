f65b1c39338616f62cad1f499a3359a780746a54
Mikolaj Cygan <miki.cyg@gmail.com>
2023-05-27 13:02:47 +0200

    Activated the Topic model through settings.py and made migrations to database and migrated the changed models.py file

--- travel_log/settings.py
+++ travel_log/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #my apps
+    "travel_logs",
+    
+    #deafault django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
