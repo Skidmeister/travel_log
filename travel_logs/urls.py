018cf7f9ffbe9e7c36492af735f7cf0ef58fec6c
Mikolaj Cygan <miki.cyg@gmail.com>
2023-06-04 20:10:16 +0200

    added links to particular trips from the page with all the trips"

--- travel_logs/urls.py
+++ travel_logs/urls.py
@@ -6,5 +6,10 @@ from . import views
 app_name = 'travel_logs'
 
 urlpatterns = [
+	#home-page
 	path('', views.index, name='index'),
+	#page with all the listed topics
+	path('topics/', views.topics, name='topics'),
+	#detail page for a single topic
+	path('topics/<int:topic_id>/', views.topic, name='topic')
 	]
--- travel_logs/views.py
+++ travel_logs/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 # Create your views here.
 
 def index(request):
 	'''Home page of travel log'''
 	return render(request, 'travel_logs/index.html')
+
+def topics(request):
+	'''Page presenting all trips that were added by the user'''
+	topics = Topic.objects.order_by('date_added')
+	context = {'topics': topics}
+	return render(request, 'travel_logs/topics.html', context)
+
+def topic(request, topic_id):
+	'''Page for a single trip with all its entries'''
+	topic = Topic.objects.get(id=topic_id)
+	entries = topic.entry_set.order_by('-date_added')
+	context = {'topic':topic, 'entries':entries}
+	return render(request, 'travel_logs/topic.html', context)
