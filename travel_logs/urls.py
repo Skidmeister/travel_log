ca3c6b87d16856d3629cb661f5718a6a8669b481
Mikolaj Cygan <miki.cyg@gmail.com>
2023-06-20 19:29:11 +0200

    Added an EDIT button to each entry of a trip.

--- travel_logs/urls.py
+++ travel_logs/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
 	path('new_topic/', views.new_topic, name='new_topic'),
 	#site to introduce a new entry for a topic
 	path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+	#editing of entries
+	path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry')
 	]
